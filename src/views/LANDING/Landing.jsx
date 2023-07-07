986aa6d2076fa8c7ac18632360d683889260a001
sofiaparraweb <sofiaparraweb@gmail.com>
2023-07-07 16:35:12 -0400

    cambios front

--- src/views/LANDING/Landing.jsx
+++ src/views/LANDING/Landing.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Landing.css'
+import './Landing.css';
 
 const Landing = () => {
   return (
     <div className="landing-page">
       <div className="background-animation"></div>
-      <div className='enter-button'>
-      <Link to="/home" >
+      <div className="enter-button">
+        <Link to="/home">
+          <span className="enter-button-icon">
             <img src="https://cdn-icons-png.flaticon.com/512/48/48821.png" alt="Back Home" />
-          </Link>
-        </div>
+          </span>
+          <span className="enter-button-text">Enter</span>
+        </Link>
+      </div>
     </div>
   );
 }
